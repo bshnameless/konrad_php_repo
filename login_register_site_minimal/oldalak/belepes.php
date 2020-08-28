668dd5c81f79866ce44654ea055525633c84fdbd
BlackStation - Mr.Nameless <hgtsoul@gmail.com>
2020-08-28 21:03:37 +0200

    lol

--- login_register_site_minimal/oldalak/belepes.php
+++ login_register_site_minimal/oldalak/belepes.php
@@ -5,15 +5,7 @@
 
  if(isset($_SESSION["belepve".$page["secretkey"]])){ header("Location: index.php"); } //ha bevan lépve ne engedje a login paget
  ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$page["title"];?></title>
-</head>
-<body>
+
     <h1>Picsafüst belépés</h1>
     <form method="POST">
         <input type="text" placeholder="ID" name="fh" />
@@ -22,5 +14,3 @@
         <br />
         <input type="submit" name="loginBtn" value="Bejelentkezés" />
     </form>
-</body>
-</html>
